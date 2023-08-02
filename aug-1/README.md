578c6450566922c447a97e86632a113fc5fe20b4
solomon-spec <solomonabate18@gmail.com>
2023-08-02 10:03:50 +0300

    final submition

--- aug-1/README.md
+++ aug-1/README.md
@@ -4,3 +4,4 @@
 2. Install llodash `touch index.js`
 3. Create index.js file `touch index.js`
 4. Add lodash in the index file ```js const lodash  = require ('lodash');```
+5. Write your code in the index file
--- aug-1/index.js
+++ aug-1/index.js
@@ -2,3 +2,7 @@ const lodash = require('lodash');
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const chunkedArray = lodash.chunk(array, 2);
 console.log(chunkedArray);
+
+array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+array2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const concatedArray = lodash.concat(array1, array2);
